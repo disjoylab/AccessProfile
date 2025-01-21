bec5ae14976dd5b3664ab937c2f54dd97d1341d2
Lloyd May <lloydmaykes@gmail.com>
2025-01-20 20:09:49 -0500

    Initial commit

---
+++ Assets/Assets/AccessibityManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AccessibityManager : MonoBehaviour
+{
+    public static AccessibityManager instance;
+    public Sprite AccessibleImage_BLACK;
+    public Sprite AccessibleImage_WHITE;
+    public Sprite AccessibleImage_BLACK_GRADIENT;
+    public Sprite AccessibleImage_WHITE_GRADIENT;
+    public Sprite AccessibleImage_BLACK_WHITE_BORDER;
+    public Sprite AccessibleImage_WHITE_BLACK_BORDER;
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+}
